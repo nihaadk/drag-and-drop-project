1a955b1db51f218f805ef372d2245b83596489eb
nihaadk <nihadkeric@gmail.com>
2020-04-14 22:31:32 +0200

    def. Draggable and DragTarget interfaces

--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,18 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 	};
 	return adjDescriptor;
 }
+
+interface Draggable {
+	dragStartHandler(event: DragEvent): void;
+	dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+	dragOverHandler(event: DragEvent): void;
+	dropHandler(event: DragEvent): void;
+	dragLeaveHandler(event: DragEvent): void;
+}
+
 // Project type
 enum ProjectStatus {
 	Active,
